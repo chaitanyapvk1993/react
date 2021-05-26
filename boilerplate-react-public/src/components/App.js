a62a116bdfc008557b60e4d8bb3000f18999b0eb
chaitanyapvk1993 <1993chaitanyapvk@gmail.com>
2021-05-26 23:11:03 +0530

    Add files via upload

---
+++ boilerplate-react-public/src/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+
+        return(
+            <div id="main">
+               {/* Do not remove the main div */}
+               <p>"I am learning React. My life is getting better. "</p>
+            </div>
+        )
+    }
+}
+
+
+export default App;
